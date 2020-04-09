ecde4c794a137ace2c44d69543c9beb3496bf15a
Lucas Mendonça <lsm5@cin.ufpe.br>
2020-04-09 03:04:13 -0300

    chore: add uuidv4 for generate id and crud

--- src/index.js
+++ src/index.js
@@ -1,9 +1,58 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
+app.use(express.json());
+
+const projects = [];
+
 app.get("/projects", (req, res) => {
-  return res.json({ message: "Hello Programmer" });
+  const { title } = req.query;
+
+  const result = title
+    ? projects.filter((project) => project.title.includes(title))
+    : projects;
+
+  return res.json(result);
+});
+
+app.post("/projects", (req, res) => {
+  const { title, owner } = req.body;
+  const project = { id: uuid(), title, owner };
+  projects.push(project);
+  return res.json(project);
+});
+
+app.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { title, owner } = req.body;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(404).json({ error: "project not found" });
+  }
+
+  const project = { id, title, owner };
+
+  projects[projectIndex] = project;
+
+  return res.json(project);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return res.status(404).json({ error: "project not found" });
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return res.status(204).send();
 });
 
 app.listen(3333, () => {
